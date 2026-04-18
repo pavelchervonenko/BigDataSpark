6a4856c0e25e462f50b44b49aa2ae36b1e4d1118
pavelchervonenko <pavel.chervonenko.97@mail.ru>
2026-04-19 01:45:00 +0300

    update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,16 +9,16 @@ repositories {
 }
 
 dependencies {
-    compileOnly("org.apache.spark:spark-core_2.12:3.5.0")
-    compileOnly("org.apache.spark:spark-sql_2.12:3.5.0")
+    compileOnly("org.apache.spark:spark-core_2.13:3.5.0")
+    compileOnly("org.apache.spark:spark-sql_2.13:3.5.0")
 
     implementation("org.postgresql:postgresql:42.7.10")
 
     implementation("com.clickhouse:clickhouse-jdbc:0.9.8:all")
 
-    implementation("com.datastax.spark:spark-cassandra-connector_2.12:3.5.0")
+    implementation("com.datastax.spark:spark-cassandra-connector_2.13:3.5.1")
 
-    implementation("org.mongodb.spark:mongo-spark-connector_2.12:10.3.0")
+    implementation("org.mongodb.spark:mongo-spark-connector_2.13:11.0.1")
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.13.4")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
